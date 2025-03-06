a982ae039ecfb020e9bfb0b82bd6d35269784a20
thanhhdct <daothanhhdct123@gmail.com>
2025-03-06 08:58:38 +0700

    update

--- scripts/get-contract-balance.ts
+++ scripts/get-contract-balance.ts
@@ -3,7 +3,7 @@ import { vars } from "hardhat/config";
 
 async function main() {
     // local
-    // const address = '0x496Cc52E280b210D7410861144B3B124d031d60D
+    // const address = '0x496Cc52E280b210D7410861144B3B124d031d60D'
     // const provider = new ethers.JsonRpcProvider("http://localhost:8545");
     // const contractBalance = await provider.getBalance(address);
     // console.log("🚀 ~ main ~ contractBalance:", contractBalance)
